06bb945b07238222afaaa4f1632b743a82bc5c67
Exeteres <exeteres@pm.me>
2024-06-22 18:04:43 +0000

    Use nixos repo for attic

--- nix/attic/flake.nix
+++ nix/attic/flake.nix
@@ -1,11 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-
-    attic = {
-      url = "github:zhaofengli/attic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
@@ -17,7 +12,7 @@
   in {
     image = pkgs.dockerTools.buildLayeredImage {
       name = "ghcr.io/exeteres/attic";
-      contents = [attic.packages.x86_64-linux.attic-server];
+      contents = [pkgs.attic-server];
     };
   };
 }
